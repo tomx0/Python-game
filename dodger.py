375918e779ad7910b265a089af1d58b315acde0a
Matej-143 <166415005+Matej-143@users.noreply.github.com>
2024-04-15 16:45:14 +0200

    Update dodger.py

--- dodger.py
+++ dodger.py
@@ -12,6 +12,15 @@ pygame.init()
 # Color
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+NAVYBLUE = (60, 60, 100)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 128, 0)
+PURPLE = (255, 0, 255)
+CYAN = (0, 255, 255)
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 #Classes
